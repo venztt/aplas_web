b1e3adfdd185c1b0a31a41a1b0f370bbdecdbc10
venztt <anthraxirfanprogamer@gmail.com>
2023-07-25 16:09:24 +0700

    update seeder

--- database/seeds/SettingTableSeeder.php
+++ database/seeds/SettingTableSeeder.php
@@ -12,9 +12,20 @@ class SettingTableSeeder extends Seeder
      */
     public function run()
     {
+        // untuk windows
+        // $settings = [[
+        //     'id' => 1,
+        //     'java_path' => 'C:\Program Files\Java\jdk1.8.0_111\bin',
+        //     'created_at' => '2022-06-27 08:27:42',
+        //     'updated_at' => '2022-06-27 08:27:42',
+        //     'deleted_at' => NULL
+        // ]];
+
+        //untuk linux
         $settings = [[
             'id' => 1,
-            'java_path' => 'C:\Program Files\Java\jdk1.8.0_111\bin',
+            'java_path' => '/usr/bin/java',
+            'java_junit_path' => '/home/noc/junit-platform-console-standalone-1.9.2.jar',
             'created_at' => '2022-06-27 08:27:42',
             'updated_at' => '2022-06-27 08:27:42',
             'deleted_at' => NULL
